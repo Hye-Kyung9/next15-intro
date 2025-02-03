ccf28545e89a998d6d790b6d727457cb31cc6de0
Hye-Kyung9 <rngprud123@naver.com>
2025-02-03 22:38:15 +0900

    feat:product/blog 페이지 생성

--- src/app/products/page.tsx
+++ src/app/products/page.tsx
@@ -1,9 +1,26 @@
-import React from 'react'
+import Link from 'next/link';
 
-const page = () => {
+export default function ProductList() {
+  const productId = 100;
+
   return (
-    <div>page</div>
-  )
+    <>
+      <Link href="/">Home</Link>
+      <h1>Product List</h1>
+      <h2>
+        <Link href="products/1">Product 1</Link>
+      </h2>
+      <h2>
+        <Link href="products/2">Product 2</Link>
+      </h2>
+      <h2>
+        <Link href="products/3" replace>
+          Product 3
+        </Link>
+      </h2>
+      <h2>
+        <Link href={`products/${productId}`}>Product {productId}</Link>
+      </h2>
+    </>
+  );
 }
-
-export default page 
