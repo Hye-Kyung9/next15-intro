e3b5ab4eaed064a8c96bf4a2b98e2ceeb5acd77c
Hye-Kyung9 <rngprud123@naver.com>
2025-03-02 23:59:24 +0900

    feat: intercepting route 경로 추가

--- src/app/f1/page.tsx
+++ src/app/f1/page.tsx
@@ -6,7 +6,13 @@ export default function F1() {
       <h1>F1 page</h1>
       <div>
         <Link href="/f1/f2">F2</Link>
+        <Link href="/f3">F3</Link>
       </div>
     </>
   );
 }
+
+// Intercepting routes conventions
+// (.) to match segments on the same level
+// (..) to match segments one level above
+// (..)(..) to match segments two levels above
